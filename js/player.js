ac49b47921194daf85a9d011b45d10c641563c17
Devidas Waghmode <waghmodedevidas121@gmail.com>
2026-09-12 15:40:20 +0530

    fix: count each game run once

--- js/player.js
+++ js/player.js
@@ -44,7 +44,6 @@ BB.Player = (function () {
   // Called by engine at game end. Returns {xp, coins} earned.
   function recordGame(mode, r) {
     var dd = d(), coins = 0;
-    dd.gamesPlayed = (dd.gamesPlayed || 0) + 1;
     if (mode === "BLITZ" && r.score > (dd.blitzHighScore || 0)) dd.blitzHighScore = r.score;
     if (mode === "INFINITE" && r.score > (dd.infiniteHighScore || 0)) dd.infiniteHighScore = r.score;
     if (mode === "SLING" && r.score > (dd.slingshotHighScore || 0)) dd.slingshotHighScore = r.score;
